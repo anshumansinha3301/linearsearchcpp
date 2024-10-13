49c6e51673c42a972876243e3964d7de522933be
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-10-13 18:21:20 +0530

    Update coderadixsort.cpp

--- coderadixsort.cpp
+++ coderadixsort.cpp
@@ -6,7 +6,7 @@ int getMax(int arr[], int size) {
     int max = arr[0];
     for (int i = 1; i < size; i++)
         if (arr[i] > max)
-            max = arr[i];
+            max = arr[i];  
     return max;
 }
 
